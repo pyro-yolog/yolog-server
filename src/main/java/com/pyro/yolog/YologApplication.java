417c3e3841b11e973fc15de3eebd788dedad0aa6
yeonjy <yeonjypaper@gmail.com>
2024-02-18 00:57:24 +0900

    feat: application settings

---
+++ src/main/java/com/pyro/yolog/YologApplication.java
@@ -0,0 +1,13 @@
+package com.pyro.yolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YologApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pyro/yolog/YologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyro.yolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YologApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
