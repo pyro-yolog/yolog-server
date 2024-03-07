8b19a3731d8522990bcab6bfdcc07d25ce6c0f16
yeonjy <yeonjypaper@gmail.com>
2024-03-07 22:07:36 +0900

    feat: Trip entity 구현

---
+++ src/main/java/com/pyro/yolog/domain/trip/entity/Trip.java
@@ -0,0 +1,29 @@
+package com.pyro.yolog.domain.trip.entity;
+
+import com.pyro.yolog.domain.member.entity.Member;
+import com.pyro.yolog.global.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Trip extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String destination;
+    private String coverImageUrl;
+    private LocalDateTime startDate;
+    private LocalDateTime finishDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
