024aede277130c0728340f915794c3c1a5ae1b13
yeonjy <yeonjypaper@gmail.com>
2024-03-13 19:40:56 +0900

    feat: 일기장 create & update & delete 로직 구현

---
+++ src/main/java/com/pyro/yolog/domain/trip/service/TripService.java
@@ -0,0 +1,36 @@
+package com.pyro.yolog.domain.trip.service;
+
+import com.pyro.yolog.domain.member.entity.Member;
+import com.pyro.yolog.domain.member.query.AuthService;
+import com.pyro.yolog.domain.trip.dto.TripRequest;
+import com.pyro.yolog.domain.trip.entity.Trip;
+import com.pyro.yolog.domain.trip.mapper.TripMapper;
+import com.pyro.yolog.domain.trip.repository.TripRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TripService {
+    private final TripRepository tripRepository;
+    private final AuthService authService;
+    private final TripMapper tripMapper;
+
+    @Transactional
+    public void saveTrip(final TripRequest request) {
+        Member login = authService.getMember();
+        tripRepository.save(tripMapper.toEntity(request, login));
+    }
+
+    @Transactional
+    public void updateTrip(final Long id, final TripRequest request) {
+        final Trip trip = tripRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        trip.update(request);
+    }
+
+    public void deleteTrip(Long id) {
+        tripRepository.deleteById(id);
+    }
+}
