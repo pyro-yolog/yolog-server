c189034bb76c88da925d236366c73161266c31ea
yeonjy <yeonjypaper@gmail.com>
2024-03-13 19:39:49 +0900

    feat: @Column(nullable = false) -> @NotNull

--- src/main/java/com/pyro/yolog/domain/trip/entity/Trip.java
+++ src/main/java/com/pyro/yolog/domain/trip/entity/Trip.java
@@ -1,8 +1,10 @@
 package com.pyro.yolog.domain.trip.entity;
 
 import com.pyro.yolog.domain.member.entity.Member;
+import com.pyro.yolog.domain.trip.dto.TripRequest;
 import com.pyro.yolog.global.config.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,10 +25,10 @@ public class Trip extends BaseTimeEntity {
     private String destination;
     private String coverImageUrl;
 
-    @Column(nullable = false)
+    @NotNull
     private LocalDateTime startDate;
 
-    @Column(nullable = false)
+    @NotNull
     private LocalDateTime finishDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -41,5 +43,13 @@ public class Trip extends BaseTimeEntity {
         this.startDate = startDate;
         this.finishDate = finishDate;
         this.member = member;
+    }
+
+    public void update(TripRequest request) {
+        this.name = request.getName();
+        this.coverImageUrl = request.getCoverImageUrl();
+        this.destination = request.getDestination();
+        this.startDate = request.getStartDate();
+        this.finishDate = request.getFinishDate();
     }
 }
