ec5e094ebc63831ddc8b953cce5f193328740ecc
yeonjy <yeonjypaper@gmail.com>
2024-03-25 02:11:02 +0900

    feat: findAllByMember 추가

--- src/main/java/com/pyro/yolog/domain/trip/repository/TripRepository.java
+++ src/main/java/com/pyro/yolog/domain/trip/repository/TripRepository.java
@@ -1,10 +1,14 @@
 package com.pyro.yolog.domain.trip.repository;
 
+import com.pyro.yolog.domain.member.entity.Member;
 import com.pyro.yolog.domain.trip.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip> findAllByMember(Member member);
 
 }
