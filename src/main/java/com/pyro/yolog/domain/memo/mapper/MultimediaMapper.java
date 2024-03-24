87ba8cbeae4aee2dbc8923c42539712e96b459d1
yeonjy <yeonjypaper@gmail.com>
2024-03-24 17:14:48 +0900

    fix: mapping source & target 지정

--- src/main/java/com/pyro/yolog/domain/memo/mapper/MultimediaMapper.java
+++ src/main/java/com/pyro/yolog/domain/memo/mapper/MultimediaMapper.java
@@ -4,10 +4,13 @@ import com.pyro.yolog.domain.memo.dto.request.MultimediaRequest;
 import com.pyro.yolog.domain.memo.entity.Memo;
 import com.pyro.yolog.domain.memo.entity.Multimedia;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MultimediaMapper {
+
+    @Mapping(source = "memo", target = "memo")
     Multimedia toEntity(Memo memo, MultimediaRequest request);
 
 }
