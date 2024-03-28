4803547c784a229b6e8e967850730ca2684be61d
yeonjy <yeonjypaper@gmail.com>
2024-03-28 13:44:51 +0900

    feat: memo 조회 & 수정 & 삭제 controller 구현

--- src/main/java/com/pyro/yolog/domain/memo/api/MemoApi.java
+++ src/main/java/com/pyro/yolog/domain/memo/api/MemoApi.java
@@ -1,7 +1,7 @@
 package com.pyro.yolog.domain.memo.api;
 
 import com.pyro.yolog.domain.memo.dto.request.MemoRequest;
-import com.pyro.yolog.domain.memo.dto.request.MultimediaRequest;
+import com.pyro.yolog.domain.memo.dto.response.MemoDetailResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -9,6 +9,9 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface MemoApi {
 
@@ -31,4 +34,27 @@ public interface MemoApi {
 
                   @RequestBody MemoRequest memoRequest
     );
+
+    @Operation(
+            summary = "빠른 메모 상세 조회",
+            description = "빠른 메모를 상세 조회합니다.",
+            security = {@SecurityRequirement(name = "access_token")},
+            tags = {"memo"}
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK"
+                    )
+            }
+    )
+    MemoDetailResponse getMemo(
+            @Parameter(in = ParameterIn.PATH, description = "일기장 ID", required = true)
+            Long tripId,
+
+            @Parameter(in = ParameterIn.PATH, description = "빠른 메모 ID", required = true)
+            Long memoId
+    );
+
 }
--- src/main/java/com/pyro/yolog/domain/memo/api/MemoController.java
+++ src/main/java/com/pyro/yolog/domain/memo/api/MemoController.java
@@ -1,11 +1,16 @@
 package com.pyro.yolog.domain.memo.api;
 
 import com.pyro.yolog.domain.memo.dto.request.MemoRequest;
+import com.pyro.yolog.domain.memo.dto.response.MemoDetailResponse;
+import com.pyro.yolog.domain.memo.dto.response.MemoPreviewResponse;
 import com.pyro.yolog.domain.memo.service.MemoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +24,31 @@ public class MemoController implements MemoApi {
     public void saveMemo(@PathVariable final Long tripId,
                          @RequestBody @Valid final MemoRequest request) {
         memoService.saveMemo(tripId, request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{memoId}")
+    @Override
+    public MemoDetailResponse getMemo(@PathVariable final Long tripId, @PathVariable final Long memoId) {
+        return memoService.getMemo(memoId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    public List<MemoPreviewResponse> getMemos(@PathVariable final Long tripId) {
+        return memoService.getMemosByTrip(tripId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{memoId}")
+    public void deleteMemo(@PathVariable final Long tripId, @PathVariable final Long memoId) {
+        memoService.deleteMemo(memoId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{memoId}")
+    public void updateMemo(@PathVariable final Long tripId, @PathVariable final Long memoId,
+                           @RequestBody @Valid final MemoRequest request) {
+        memoService.updateMemo(memoId, request);
     }
 }
