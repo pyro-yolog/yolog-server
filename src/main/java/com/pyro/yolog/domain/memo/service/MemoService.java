2b6228ba18c4f00cbe195124cc8a67a042c36a01
yeonjy <yeonjypaper@gmail.com>
2024-03-28 13:53:57 +0900

    feat: Multimedia service 분리 & updateMemo & getMemo 구현

--- src/main/java/com/pyro/yolog/domain/memo/service/MemoService.java
+++ src/main/java/com/pyro/yolog/domain/memo/service/MemoService.java
@@ -1,34 +1,65 @@
 package com.pyro.yolog.domain.memo.service;
 
+import com.pyro.yolog.domain.memo.dto.MultimediaDto;
 import com.pyro.yolog.domain.memo.dto.request.MemoRequest;
+import com.pyro.yolog.domain.memo.dto.response.MemoDetailResponse;
+import com.pyro.yolog.domain.memo.dto.response.MemoPreviewResponse;
 import com.pyro.yolog.domain.memo.entity.Memo;
 import com.pyro.yolog.domain.memo.mapper.MemoMapper;
-import com.pyro.yolog.domain.memo.mapper.MultimediaMapper;
 import com.pyro.yolog.domain.memo.repository.MemoRepository;
-import com.pyro.yolog.domain.memo.repository.MultimediaRepository;
 import com.pyro.yolog.domain.trip.service.TripService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemoService {
     private final MemoRepository memoRepository;
-    private final MultimediaRepository multimediaRepository;
     private final MemoMapper memoMapper;
-    private final MultimediaMapper multimediaMapper;
     private final TripService tripService;
+    private final MultimediaService multimediaService;
 
     @Transactional
     public void saveMemo(final Long tripId, final MemoRequest request) {
-        if (request.getTitle().isBlank()
-                && request.getMultimediaRequest().getContentUrl().isBlank()) {
+        if (isNoContent(request)) {
             return;
         }
         final Memo memo = memoRepository.save(
                 memoMapper.toEntity(tripService.getTrip(tripId), request));
-        multimediaRepository.save(multimediaMapper.toEntity(memo, request.getMultimediaRequest()));
+        multimediaService.saveMultimedias(memo, request.getMultimediaDtos());
     }
-    
+
+    private boolean isNoContent(MemoRequest request) {
+        return request.getTitle().isBlank() && request.getMultimediaDtos().isEmpty();
+    }
+
+    public MemoDetailResponse getMemo(Long memoId) {
+        List<MultimediaDto> multimediaDtos = multimediaService.getMultimedias(memoId);
+        return memoMapper.toDetailResponse(memoRepository.findById(memoId)
+                .orElseThrow(EntityNotFoundException::new), multimediaDtos);
+
+    }
+
+    public void deleteMemo(Long memoId) {
+        memoRepository.deleteById(memoId);
+    }
+
+    @Transactional
+    public void updateMemo(Long memoId, @Valid MemoRequest request) {
+        if (isNoContent(request)) {
+            return;
+        }
+        final Memo memo = memoRepository.findById(memoId).orElseThrow(EntityNotFoundException::new);
+        memo.update(request);
+        multimediaService.deleteMultimedias(memo);
+        multimediaService.saveMultimedias(memo, request.getMultimediaDtos());
+    }
+
 }
