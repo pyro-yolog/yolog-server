5c039191f5dc9566a698e7da3fd2ed346469631e
yeonjy <yeonjypaper@gmail.com>
2024-04-11 19:36:41 +0900

    feat: Trip 전체 조회 Swagger 구현

--- src/main/java/com/pyro/yolog/domain/trip/api/TripApi.java
+++ src/main/java/com/pyro/yolog/domain/trip/api/TripApi.java
@@ -1,6 +1,8 @@
 package com.pyro.yolog.domain.trip.api;
 
+import com.pyro.yolog.domain.memo.dto.response.MemoPreviewResponse;
 import com.pyro.yolog.domain.trip.dto.TripRequest;
+import com.pyro.yolog.domain.trip.dto.TripResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -8,7 +10,8 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface TripApi {
 
@@ -61,4 +64,21 @@ public interface TripApi {
             @Parameter(in = ParameterIn.QUERY, description = "일기장 ID", required = true)
             Long id
     );
+
+    @Operation(
+            summary = "일기장 전체 조회",
+            description = "회원별 일기장을 모두 조회합니다.",
+            security = {@SecurityRequirement(name = "access_token")},
+            tags = {"trip"}
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK"
+                    )
+            }
+    )
+    List<TripResponse> getTrips();
+
 }
