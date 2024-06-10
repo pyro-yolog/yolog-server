c1aabcd30cd280828ade2f08ea7b3f50fcb0710a
yeonjy <yeonjypaper@gmail.com>
2024-06-11 01:43:11 +0900

    refactor: MemberFixture 적용

--- src/test/java/com/pyro/yolog/domain/global/LoginTest.java
+++ src/test/java/com/pyro/yolog/domain/global/LoginTest.java
@@ -1,13 +1,14 @@
-package com.pyro.yolog.support.database;
+package com.pyro.yolog.domain.global;
 
 import com.pyro.yolog.domain.member.entity.Member;
 import com.pyro.yolog.domain.member.query.AuthService;
 import com.pyro.yolog.domain.member.repository.MemberRepository;
-import com.pyro.yolog.fixture.member.MemberEntitiyFixture;
+import com.pyro.yolog.support.database.DatabaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static com.pyro.yolog.fixture.member.MemberFixture.MEMBER;
 import static org.mockito.Mockito.when;
 
 @DatabaseTest
@@ -22,7 +23,7 @@ public abstract class LoginTest {
 
     @BeforeEach
     public void setup() {
-        loginUser = memberRepository.save(MemberEntitiyFixture.LOGIN_MEMBER.toEntity());
+        loginUser = memberRepository.save(MEMBER());
         when(authService.getLoginUserId()).thenReturn(loginUser.getId());
         when(authService.getLoginUser()).thenReturn(loginUser);
     }
