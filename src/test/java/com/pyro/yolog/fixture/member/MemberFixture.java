4d37bbe3e2c8e022ff570fb2f52553c35099f618
yeonjy <yeonjypaper@gmail.com>
2024-06-11 01:41:25 +0900

    refactor: MemberFixture 구현

---
+++ src/test/java/com/pyro/yolog/fixture/member/MemberFixture.java
@@ -0,0 +1,23 @@
+package com.pyro.yolog.fixture.member;
+
+import com.pyro.yolog.domain.member.entity.Member;
+import com.pyro.yolog.domain.member.entity.Role;
+import com.pyro.yolog.domain.member.entity.SocialType;
+import com.pyro.yolog.domain.member.entity.Status;
+import lombok.RequiredArgsConstructor;
+
+public class MemberFixture {
+    public final static Member MEMBER() {
+        return Member.builder()
+                .oauthId("12345")
+                .nickname("이면지")
+                .email("paper@daum.net")
+                .password("12345678")
+                .imageUrl("http://onesideusedpaper")
+                .refreshToken("HiruByeru")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .status(Status.ACTIVE)
+                .build();
+    }
+}
