aef943050b3fffd30a6cb2a6594499b11e3a905b
yeonjy <yeonjypaper@gmail.com>
2024-06-11 12:59:47 +0900

    feat: SignUp api 구현

---
+++ src/main/java/com/pyro/yolog/domain/member/dto/SignUpRequest.java
@@ -0,0 +1,10 @@
+package com.pyro.yolog.domain.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class SignUpRequest {
+    private String nickname;
+}
