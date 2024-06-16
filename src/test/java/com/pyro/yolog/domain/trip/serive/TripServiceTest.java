42d6a74a223073e1d20f31b1ff91f1be3a2dfebb
yeonjy <yeonjypaper@gmail.com>
2024-06-16 17:48:06 +0900

    feat: trip 조회 테스트 로직 추가

--- src/test/java/com/pyro/yolog/domain/trip/serive/TripServiceTest.java
+++ src/test/java/com/pyro/yolog/domain/trip/serive/TripServiceTest.java
@@ -1,15 +1,10 @@
 package com.pyro.yolog.domain.trip.serive;
 
-import com.pyro.yolog.domain.global.BaseControllerTest;
 import com.pyro.yolog.domain.global.LoginTest;
-import com.pyro.yolog.domain.member.entity.Member;
-import com.pyro.yolog.domain.member.query.AuthService;
 import com.pyro.yolog.domain.trip.dto.TripRequest;
 import com.pyro.yolog.domain.trip.entity.Trip;
-import com.pyro.yolog.domain.trip.mapper.TripMapper;
 import com.pyro.yolog.domain.trip.repository.TripRepository;
 import com.pyro.yolog.domain.trip.service.TripService;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,19 +27,17 @@ class TripServiceTest extends LoginTest {
     @Autowired
     private TripRepository tripRepository;
 
-    private Member member;
     private Trip trip;
 
     @BeforeEach
     void setUp() {
-        member = MEMBER();
-        trip = TRIP(member);
+        trip = TRIP(MEMBER());
     }
 
 
     @Test
     @DisplayName("Trip을 생성할 수 있는가")
-    void createTrip() {
+    void saveTrip() {
         //given
         TripRequest request = TripRequest.builder()
                 .name(trip.getName())
@@ -92,5 +85,18 @@ class TripServiceTest extends LoginTest {
 
         //then
         assertThat(tripRepository.findById(id).isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Trip을 ID로 조회할 수 있는가")
+    void getTrip() {
+        //given
+        Long id = tripRepository.save(trip).getId();
+
+        //when
+        Trip result = tripService.getTrip(id);
+
+        //then
+        assertThat(result).isEqualTo(trip);
     }
 }
