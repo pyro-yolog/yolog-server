70cc563ce1a13c6ae239ea20dc482018fc9e5cd2
yeonjy <yeonjypaper@gmail.com>
2024-06-23 06:10:21 +0900

    feat: S3이미지 업로드 & 삭제 로직 구현

---
+++ src/main/java/com/pyro/yolog/global/s3/service/S3ImageService.java
@@ -0,0 +1,77 @@
+package com.pyro.yolog.global.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.pyro.yolog.global.s3.exception.FileUploadFailureException;
+import com.pyro.yolog.global.s3.exception.InvalidFileExtensionException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3ImageService {
+
+    @Value("${cloud.aws.s3.bucket-name}")
+    private String BUCKET_NAME;
+
+    private final AmazonS3 amazonS3;
+
+    public void uploadCover(MultipartFile file, String fileName) {
+        validateImageExtension(fileName);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + fileName;
+
+        try {
+            amazonS3.putObject(BUCKET_NAME + "/trip", s3FileName, file.getInputStream(), metadata);
+        } catch (IOException e) {
+            throw new FileUploadFailureException();
+        }
+    }
+
+    private void validateImageExtension(String fileName) {
+        int lastDotIndex = getLastDotIndex(fileName);
+        String extention = fileName.substring(lastDotIndex + 1).toLowerCase();
+        List<String> allowedExtentionList = Arrays.asList("jpg", "jpeg", "png", "gif");
+
+        if (!allowedExtentionList.contains(extention)) {
+            throw new InvalidFileExtensionException();
+        }
+    }
+
+    private int getLastDotIndex(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            throw new InvalidFileExtensionException();
+        }
+        return lastDotIndex;
+    }
+
+    public void delete(String imageAddress) {
+        String key = getKeyFromImageAddress(imageAddress);
+        amazonS3.deleteObject(new DeleteObjectRequest(BUCKET_NAME, key));
+    }
+
+    private String getKeyFromImageAddress(String imageAddress){
+        try{
+            URL url = new URL(imageAddress);
+            String decodingKey = URLDecoder.decode(url.getPath(), StandardCharsets.UTF_8);
+            return decodingKey.substring(1); // 맨 앞의 '/' 제거
+        }catch (MalformedURLException e){
+            throw new FileUploadFailureException();
+        }
+    }
+}
