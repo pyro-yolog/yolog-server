dc3025ab2cd8d8911bfb89285b3863f1c6c09f73
yeonjy <yeonjypaper@gmail.com>
2024-06-23 16:29:28 +0900

    feat: 회원가입 API 반환 타입 수정

--- src/main/java/com/pyro/yolog/domain/member/api/SignApi.java
+++ src/main/java/com/pyro/yolog/domain/member/api/SignApi.java
@@ -10,16 +10,16 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 public interface SignApi {
     @Operation(
             summary = "회원가입",
-            description = "사용자의 닉네임을 입력받아 회원가입을 진행합니다.",
+            description = "사용자의 닉네임을 입력받아 회원가입을 진행합니다. 중복된 닉네임이 입력되면 false를 반환합니다.",
             security = {@SecurityRequirement(name = "access_token")},
             tags = {"로그인"}
     )
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "200",
+                    responseCode = "201",
                     description = "회원가입이 성공적으로 완료되었습니다."
             )
     })
-    void signUp(@RequestBody SignUpRequest dto);
+    boolean signUp(@RequestBody SignUpRequest dto);
 
 }
