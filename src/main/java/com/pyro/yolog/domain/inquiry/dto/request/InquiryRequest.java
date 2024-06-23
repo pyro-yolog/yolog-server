629ff58623a9310333cb83a0c05ff149ffb9945e
yeonjy <yeonjypaper@gmail.com>
2024-06-23 23:44:49 +0900

    docs: dto 설명 추가

--- src/main/java/com/pyro/yolog/domain/inquiry/dto/request/InquiryRequest.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/dto/request/InquiryRequest.java
@@ -1,6 +1,6 @@
 package com.pyro.yolog.domain.inquiry.dto.request;
 
-import com.pyro.yolog.domain.inquiry.entity.Inquiry;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -12,9 +12,11 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class InquiryRequest {
+    @Schema(description = "문의 내용입니다. 공백으로 제출하면 안됩니다.")
     @NotBlank
     private String content;
 
+    @Schema(description = "문의사항에 참고되는 이미지 URL 입니다.")
     private List<String> imageUrls;
 
 }
