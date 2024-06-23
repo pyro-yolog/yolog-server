8c43b687eee93413fd80ca62a0f9e39ca1198726
yeonjy <yeonjypaper@gmail.com>
2024-06-24 00:31:18 +0900

    feat: InquiryRepository 조회 메서드 추가

--- src/main/java/com/pyro/yolog/domain/inquiry/repository/InquiryRepository.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/repository/InquiryRepository.java
@@ -1,9 +1,13 @@
 package com.pyro.yolog.domain.inquiry.repository;
 
 import com.pyro.yolog.domain.inquiry.entity.Inquiry;
+import com.pyro.yolog.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+    List<Inquiry> findAllByMember(Member member);
 }
