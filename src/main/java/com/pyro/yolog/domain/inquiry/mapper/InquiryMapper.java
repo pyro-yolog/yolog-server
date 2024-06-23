723cd012af649d9fc3c7a80c1843893da33b4e89
yeonjy <yeonjypaper@gmail.com>
2024-06-23 23:45:28 +0900

    feat: Inquiry 관련 toEntity mapper 추가

---
+++ src/main/java/com/pyro/yolog/domain/inquiry/mapper/InquiryMapper.java
@@ -0,0 +1,11 @@
+package com.pyro.yolog.domain.inquiry.mapper;
+
+import com.pyro.yolog.domain.inquiry.entity.Inquiry;
+import com.pyro.yolog.domain.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface InquiryMapper {
+    Inquiry toEntity(Member member, String content);
+}
