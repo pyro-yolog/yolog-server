310c5fdc5fd8219b715eb2359ed51ff026bac955
yeonjy <yeonjypaper@gmail.com>
2024-07-02 00:58:48 +0900

    refactor: 필요없는 import 제거

--- src/main/java/com/pyro/yolog/domain/inquiry/dto/response/InquiryPreview.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/dto/response/InquiryPreview.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
