be742a463355e1063434571fe42b22845663b580
yeonjy <yeonjypaper@gmail.com>
2024-07-02 00:57:34 +0900

    feat: createdAt에 JsonFormat 적용

--- src/main/java/com/pyro/yolog/domain/inquiry/dto/response/DetailInquiryResponse.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/dto/response/DetailInquiryResponse.java
@@ -1,8 +1,10 @@
 package com.pyro.yolog.domain.inquiry.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -10,10 +12,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class DetailInquiryResponse {
+    @Schema(description = "문의 ID")
     private Long id;
     private String title;
     private String content;
+    @Schema(description = "문의에 대한 답변")
     private String answer;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
+    @Schema(description = "답변 완료되었는지 여부")
     private Boolean isAnswered;
 }
