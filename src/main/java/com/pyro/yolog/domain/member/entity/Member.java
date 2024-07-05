641748a562d68648153dfe443b3046f658e072a0
yeonjy <yeonjypaper@gmail.com>
2024-07-06 03:35:54 +0900

    fix: 자잘한 오류 수정

--- src/main/java/com/pyro/yolog/domain/member/entity/Member.java
+++ src/main/java/com/pyro/yolog/domain/member/entity/Member.java
@@ -32,6 +32,6 @@ public class Member extends BaseTimeEntity {
 
     public void signUp(SignUpRequest dto) {
         this.nickname = dto.getNickname();
-        this.role = Role.GUEST;
+        this.role = Role.USER;
     }
 }
