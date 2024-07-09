2cd3861ccb899bf487784910124fef975d5872dc
yeonjy <yeonjypaper@gmail.com>
2024-07-09 23:49:09 +0900

    feat: 이미 존재하는 회원 login redirect uri 수정

--- src/main/java/com/pyro/yolog/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/pyro/yolog/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -28,7 +28,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         if(oAuth2User.getRole() == Role.GUEST) {
             String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
             response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-            response.sendRedirect("/social-login?Authorization=" + accessToken);
+            response.sendRedirect("/social-login?exist=false&Authorization=" + accessToken);
 
             jwtService.sendAccessAndRefreshToken(response, accessToken, null);
         } else {
@@ -47,7 +47,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
-        return "/?Authorization=" + accessToken + "&Authorization-Refresh=" + refreshToken;
+        return "/social-login?exist=true&Authorization=" + accessToken + "&Authorization-Refresh=" + refreshToken;
     }
 }
 
