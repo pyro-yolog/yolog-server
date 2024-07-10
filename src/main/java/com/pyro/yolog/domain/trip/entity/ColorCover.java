1b916774e5ad9ef91665a0bd41fd78f899f63cef
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-07-11 02:54:39 +0900

    fix: 색상코드에 '#' 추가

--- src/main/java/com/pyro/yolog/domain/trip/entity/ColorCover.java
+++ src/main/java/com/pyro/yolog/domain/trip/entity/ColorCover.java
@@ -10,22 +10,22 @@ import java.util.stream.Stream;
 @Getter
 @RequiredArgsConstructor
 public enum ColorCover {
-    PALE_YELLOW("FFFFB5"),
-    AQUA_BLUE("D2F5F5"),
-    PASTEL_BLUE("C6DCE9"),
-    GRAY_GREEN("9DAAA2"),
-    PALE_MINT("C6EFDC"),
-    PALE_PEACH("FED5CF"),
-    BABY_PINK("FFD1DB"),
-    BEIGE_BROWN("BBA498"),
-    IVORY("F2E3C7"),
-    PALE_OLIVE_GREEN("C4D4B1"),
-    PALE_GREEN("B2BDA8"),
-    LIGHT_BROWN("A49D92"),
-    PALE_AQUA("C6DBDA"),
-    PALE_CORAL("F1B598"),
-    LAVENDER("D3C7E6"),
-    PALE_LILAC("ECD5E3")
+    PALE_YELLOW("#FFFFB5"),
+    AQUA_BLUE("#D2F5F5"),
+    PASTEL_BLUE("#C6DCE9"),
+    GRAY_GREEN("#9DAAA2"),
+    PALE_MINT("#C6EFDC"),
+    PALE_PEACH("#FED5CF"),
+    BABY_PINK("#FFD1DB"),
+    BEIGE_BROWN("#BBA498"),
+    IVORY("#F2E3C7"),
+    PALE_OLIVE_GREEN("#C4D4B1"),
+    PALE_GREEN("#B2BDA8"),
+    LIGHT_BROWN("#A49D92"),
+    PALE_AQUA("#C6DBDA"),
+    PALE_CORAL("#F1B598"),
+    LAVENDER("#D3C7E6"),
+    PALE_LILAC("#ECD5E3")
     ;
 
     private final String code;
