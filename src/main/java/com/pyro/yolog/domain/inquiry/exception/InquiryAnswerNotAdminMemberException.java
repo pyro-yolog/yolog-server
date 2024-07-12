03b29060f7f63194a3092cb584e12f515b2c7cfa
yeonjy <yeonjypaper@gmail.com>
2024-07-12 10:39:51 +0900

    rename: INQUIRY_NOT_ADMIN_MEMBER_ERROR 로 변경

--- src/main/java/com/pyro/yolog/domain/inquiry/exception/InquiryAnswerNotAdminMemberException.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/exception/InquiryAnswerNotAdminMemberException.java
@@ -5,6 +5,6 @@ import com.pyro.yolog.global.error.exception.BusinessException;
 
 public class InquiryAnswerNotAdminMemberException extends BusinessException {
     public InquiryAnswerNotAdminMemberException() {
-        super(ErrorCode.INQUIRY_NOT_ADMIN_MEMBER);
+        super(ErrorCode.INQUIRY_NOT_ADMIN_MEMBER_ERROR);
     }
 }
