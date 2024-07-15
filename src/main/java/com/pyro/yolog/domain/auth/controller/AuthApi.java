8585b97d4e8e4be2884849ec9d5cad8aaf0ef07c
yeonjy <yeonjypaper@gmail.com>
2024-07-15 20:08:03 +0900

    refactor: sign in -> auth package

--- src/main/java/com/pyro/yolog/domain/auth/controller/AuthApi.java
+++ src/main/java/com/pyro/yolog/domain/auth/controller/AuthApi.java
@@ -1,8 +1,8 @@
 package com.pyro.yolog.domain.auth.controller;
 
+import com.pyro.yolog.domain.member.dto.request.SignUpRequest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -10,6 +10,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Auth")
 public interface AuthApi {
+
+    @Operation(
+            summary = "회원가입",
+            description = "사용자의 닉네임을 입력받아 회원가입을 진행합니다. 중복된 닉네임이 입력되면 false를 반환합니다.",
+            security = {@SecurityRequirement(name = "access_token")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "회원가입이 성공적으로 완료되었습니다."
+            )
+    })
+    boolean signUp(@RequestBody SignUpRequest dto);
+
 
     @Operation(
             summary = "회원 탈퇴",
