7bdf523e9d557ee773dcb2a405ea8118286bab9d
yeonjy <yeonjypaper@gmail.com>
2024-08-22 02:08:35 +0900

    feat: inquiry image cascade 삭제

--- src/main/java/com/pyro/yolog/domain/inquiry/entity/Inquiry.java
+++ src/main/java/com/pyro/yolog/domain/inquiry/entity/Inquiry.java
@@ -7,7 +7,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -26,6 +27,9 @@ public class Inquiry extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @OneToMany(mappedBy = "inquiry_image", cascade = CascadeType.ALL)
+    private List<Inquiry> inquiries;
 
     @Builder
     public Inquiry(String title, String content, Member member) {
