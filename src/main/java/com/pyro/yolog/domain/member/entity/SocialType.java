18c9d98999a847a643f0e27b1da8d04d86f2beb1
yeonjy <yeonjypaper@gmail.com>
2024-09-27 18:53:43 +0900

    feat: social type google 추가

--- src/main/java/com/pyro/yolog/domain/member/entity/SocialType.java
+++ src/main/java/com/pyro/yolog/domain/member/entity/SocialType.java
@@ -1,8 +1,19 @@
 package com.pyro.yolog.domain.member.entity;
 
-public enum SocialType {
-    APPLE,
-    GOOGLE,
-    KAKAO
-}
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
+public enum SocialType {
+    GOOGLE("https://www.googleapis.com/oauth2/v2/userinfo"),
+    KAKAO("https://kapi.kakao.com/v2/user/me");
+
+    private final String providerUrl;
+
+    @JsonCreator
+    public static SocialType from(String s) {
+        return SocialType.valueOf(s.toUpperCase());
+    }
+}
