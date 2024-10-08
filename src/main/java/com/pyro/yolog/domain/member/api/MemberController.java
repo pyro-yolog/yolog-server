4c3f0f175df3b4acbb5c20f65aba59941355c5ce
yeonjy <yeonjypaper@gmail.com>
2024-10-08 20:12:10 +0900

    feat: Member Role 조회 api 구현

--- src/main/java/com/pyro/yolog/domain/member/api/MemberController.java
+++ src/main/java/com/pyro/yolog/domain/member/api/MemberController.java
@@ -1,6 +1,7 @@
 package com.pyro.yolog.domain.member.api;
 
-import com.pyro.yolog.domain.member.dto.response.SocialTypeResponse;
+import com.pyro.yolog.domain.member.dto.response.MemberRoleResponse;
+import com.pyro.yolog.domain.member.dto.response.MemberSocialTypeResponse;
 import com.pyro.yolog.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,12 +10,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("members")
-public class MemberController {
+public class MemberController implements MemberApi{
     private final MemberService memberService;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("social-type")
-    public SocialTypeResponse getSocialType() {
+    @GetMapping("/role")
+    public MemberRoleResponse getRole() {
+        return memberService.getRole();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/social-type")
+    @Override
+    public MemberSocialTypeResponse getSocialType() {
         return memberService.getSocialType();
     }
 }
