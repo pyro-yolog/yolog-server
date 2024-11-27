5e9d8ca67be25e311d9ec42d20e4a30e69ee39a1
yeonjy <yeonjypaper@gmail.com>
2024-11-28 00:26:34 +0900

    fix: oauth id (sub -> id)

--- src/main/java/com/pyro/yolog/global/oauth2/userInfo/GoogleOAuth2UserInfo.java
+++ src/main/java/com/pyro/yolog/global/oauth2/userInfo/GoogleOAuth2UserInfo.java
@@ -10,7 +10,7 @@ public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
 
     @Override
     public String getId() {
-        return (String)attributes.get("sub");
+        return (String)attributes.get("id");
     }
 
     @Override
